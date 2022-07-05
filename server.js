1ef593f57e334ea1b8f0ca0b3b5a1a48d90e7b72
eslamdahshan61 <eslamdahshan61@gmail.com>
2022-07-05 20:20:19 +0200

    dd

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const app = express();
 app.use(express.static('./dist'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist'}),
+    /* res.sendFile('index.html', {root: 'dist'}), */
+    res.sendFile(path.join(__dirname + '/dist/index.html'))
 );
 
 // Start the app by listening on the default Heroku port
