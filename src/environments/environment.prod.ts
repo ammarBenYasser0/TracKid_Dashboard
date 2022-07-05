93c6a773b83314f91155353757334080bf33aff7
eslamdahshan61 <eslamdahshan61@gmail.com>
2022-07-05 16:14:52 +0200

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'https://krombo.000webhostapp.com/api/',
+  api: 'http://kidtrack.herokuapp.com/api/',
   secretKey: 'af1cc4c6ab3fdd5a69b99e46ec4a0cc6',
 };
