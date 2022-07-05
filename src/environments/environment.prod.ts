7699e32463ef0d0affe8c0b0f8f5032f1b4633da
eslamdahshan61 <eslamdahshan61@gmail.com>
2022-07-05 16:36:28 +0200

    Prod HTTPS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'http://kidtrack.herokuapp.com/api/',
+  api: 'https://kidtrack.herokuapp.com/api/',
   secretKey: 'af1cc4c6ab3fdd5a69b99e46ec4a0cc6',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api: 'http://kidtrack.herokuapp.com/api/',
+  api: 'https://kidtrack.herokuapp.com/api/',
   secretKey: 'af1cc4c6ab3fdd5a69b99e46ec4a0cc6',
 };
 
